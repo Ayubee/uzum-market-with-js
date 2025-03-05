bce9a3d888980b55c8d4fe70466ba9d8a586e31c
Ayubxon <aubsattorov@gmail.com>
2025-03-05 12:31:56 +0500

    initialize project structure with essential files and configurations

---
+++ src/js/product.js
@@ -0,0 +1,12 @@
+import { fetchData } from "./fetchData.js";
+import { showProduct } from "./updateUI.js";
+const queryString = window.location.search;
+const id = new URLSearchParams(queryString).get("id");
+
+fetchData("https://dummyjson.com/product/" + id)
+  .then((product) => {
+    showProduct(product);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
